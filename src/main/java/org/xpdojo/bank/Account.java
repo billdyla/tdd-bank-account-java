9ee094a51344e7a52b91bbb08a704a7c3e56a60b
Bill Dyla <bill.dyla@gmail.com>
2022-01-28 08:44:52 -0500

    add increment, decrement and transfer

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,28 @@
 package org.xpdojo.bank;
 
+import static java.lang.Math.abs;
+
 public class Account {
+    private int _balance;
+    public static Account newAccount() {
+        return new Account();
+    }
+
+    private Account() {
+        _balance = 0;
+    }
+
+    public int transact(int transaction) {
+        _balance += transaction;
+        return _balance;
+    }
+
+    public void transfer(Account source, Account target, int transaction) {
+        source.transact(-1 * abs(transaction) );
+        target.transact(abs(transaction));
+    }
+
+    public int balance() {
+        return _balance;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -4,12 +4,27 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.xpdojo.bank.Account.newAccount;
 
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void check_new_account() {
+        assertThat(newAccount().balance() == 0);
+    }
+
+    @Test
+    public void check_transation() {
+        int transaction = 10;
+        assertThat(newAccount().transact(transaction) == transaction);
+    }
+
+    @Test
+    public void check_transfer() {
+        Account source = newAccount();
+        source.transact(10);
+        Account target = newAccount();
+        assertThat(source.balance() == 5);
+        assertThat(target.balance() == 5);
     }
 }
